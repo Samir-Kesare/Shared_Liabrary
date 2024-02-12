4399d9335b21896c9151ac0a10bf8462520a0e1f
Harshit Singh <156056444+Panu-S-Harshit-Ninja-07@users.noreply.github.com>
2024-02-12 16:54:37 +0530

    Update gitCheckout.groovy

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,1 +1,7 @@
-
+def call(Map config = [:]) {
+            checkout scm: [
+                $class: 'GitSCM',
+                branches: [[name: ${config.branch}]],
+                userRemoteConfigs: [[url: ${config.url}]]
+            ]
+}
