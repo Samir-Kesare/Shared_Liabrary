867de815d09912418a964924965335d1273157a0
shreya-snaatak <156057205+shreya-snaatak@users.noreply.github.com>
2024-02-12 22:46:03 +0530

    Update poststeps.groovy

--- vars/poststeps.groovy
+++ vars/poststeps.groovy
@@ -1,1 +1,30 @@
-
+def call() {
+    return {
+        post {
+            success {
+                script {
+                    // Send Slack notification for success
+                    slackSend(
+                        color: '#36a64f',
+                        message: "Declarative Job completed successfully!",
+                        channel: "#jenkins",
+                        teamDomain: "demoworkspace-6868926",
+                        tokenCredentialId: "e96c6c7f-1fdf-4c4a-80fd-5ad178092678"
+                    )
+                }
+            }
+            failure {
+                script {
+                    // Send Slack notification for failure
+                    slackSend(
+                        color: '#ff0000',
+                        message: "Declarative Job failed!",
+                        channel: "#jenkins",
+                        teamDomain: "demoworkspace-6868926",
+                        tokenCredentialId: "e96c6c7f-1fdf-4c4a-80fd-5ad178092678"
+                    )
+                }
+            }
+        }
+    }
+}
