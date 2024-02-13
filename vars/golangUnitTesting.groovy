8da794e045757648a7b733fe4def3c2f0a04434a
Vishalkk1998 <156056413+Vishalkk1998@users.noreply.github.com>
2024-02-14 00:06:16 +0530

    Update golangUnitTesting.groovy

--- vars/golangUnitTesting.groovy
+++ vars/golangUnitTesting.groovy
@@ -1,1 +1,17 @@
+def installgo(){
+  // Update apt packages
+  sh 'sudo apt update' 
+  // Install Go using snap
+  sh 'sudo snap install go --classic'  
+}
 
+def testing(){
+  // Run go test and ignore errors
+  sh 'go test ./... || true'
+}
+
+def html(){
+  // Run gotest with specify the output format and generate HTML Report
+  sh 'go test ./... -coverprofile=coverage.out || true'
+  sh 'go tool cover -html=coverage.out -o coverage.html || true'
+}
