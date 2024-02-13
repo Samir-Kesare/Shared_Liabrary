1413a118735e188308d3087dd91a56e71adcf3e8
Vishalkk1998 <156056413+Vishalkk1998@users.noreply.github.com>
2024-02-13 23:49:35 +0530

    Create gitCheckoutPrivate.groovy

---
+++ vars/gitCheckoutPrivate.groovy
@@ -0,0 +1,8 @@
+// Clone Private Repo
+def call(Map config = [:]) {
+    checkout scm: [
+        $class: 'GitSCM',
+        branches: [[name: config.branch]],
+        userRemoteConfigs: [[url: config.url, credentialsId: config.credentialsId]]
+    ]
+}
