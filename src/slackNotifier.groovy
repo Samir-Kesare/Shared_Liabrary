115b38fcd4f03de09749591ba8fa8ea9b0d82b13
shreya-snaatak <156057205+shreya-snaatak@users.noreply.github.com>
2024-02-13 22:16:30 +0530

    Update slackNotifier.groovy

--- src/slackNotifier.groovy
+++ src/slackNotifier.groovy
@@ -1,14 +1,14 @@
-// src/SlackNotifier.groovy
+// src/slackNotifier.groovy
 
-class SlackNotifier {
-    static void slackNotification(String status) {
-        if (status == 'SUCCESS') {
-            slackSend channel: 'jenkinss', color: '#00FF00', message: 'Job Build successfully'
-        }
-        if (status == 'FAILURE') {
-            slackSend channel: 'jenkinss', color: '#FF0000', message: 'Job Failed'
-        }
+def slackNotification(String status) {
+    if (status == 'SUCCESS') {
+        slackSend channel: 'jenkinss', color: '#00FF00', message: 'Job Build successfully'
+    } else if (status == 'FAILURE') {
+        slackSend channel: 'jenkinss', color: '#FF0000', message: 'Job Failed'
+    } else {
+        echo "Unknown status: $status"
     }
 }
+
 
 
