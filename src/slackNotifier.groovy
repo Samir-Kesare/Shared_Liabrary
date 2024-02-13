61117f069ca1d9bed229c7117356409d2055dcfe
shreya-snaatak <156057205+shreya-snaatak@users.noreply.github.com>
2024-02-13 21:47:38 +0530

    Create slackNotifier.groovy

---
+++ src/slackNotifier.groovy
@@ -0,0 +1,12 @@
+// src/slackNotifier.groovy
+
+class slackNotifier {
+    static void slackNotification(String status) {
+        if (status == 'SUCCESS') {
+            slackSend channel: 'jenkinss', color: '#00FF00', message: 'Job Build successfully'
+        }
+        if (status == 'FAILURE') {
+            slackSend channel: 'jenkinss', color: '#FF0000', message: 'Job Failed'
+        }
+    }
+}
