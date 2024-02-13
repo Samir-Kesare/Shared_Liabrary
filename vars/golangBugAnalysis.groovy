1cb5cff7b345bbc0353c4b2836f99c5a228b4403
Vishalkk1998 <156056413+Vishalkk1998@users.noreply.github.com>
2024-02-14 01:22:48 +0530

    Update golangBugAnalysis.groovy

--- vars/golangBugAnalysis.groovy
+++ vars/golangBugAnalysis.groovy
@@ -1,1 +1,22 @@
+def installpre-requisites(){
+  // Update apt packages
+  sh 'sudo apt update'
+  // Install Go using snap
+  sh 'sudo snap install go --classic'
+  // Remove golangci-lint using snap
+  sh 'sudo snap remove golangci-lint || true'
+  // Install GolangCI-lint
+  sh 'go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest'
+  // Add $HOME/go/bin to PATH
+  env.PATH += ":$HOME/go/bin"
+}
 
+def linting(){
+  // Run golangci-lint and ignore errors
+  sh 'golangci-lint run ./... || true'
+}
+
+def html(){
+  // Run golangci-lint with the --out-format option to specify the output format
+  sh 'golangci-lint run ./... --out-format html > report.html || true'
+}
