64c5e887e7ac71bb1c344285979614bb78588090
Harshit Singh <156056444+Panu-S-Harshit-Ninja-07@users.noreply.github.com>
2024-02-13 11:51:39 +0530

    Update slackNotification.groovy

--- vars/slackNotification.groovy
+++ vars/slackNotification.groovy
@@ -1,7 +1,10 @@
 // vars/postSteps.groovy
 
-def call() {
-    return this
+def call(String status) {
+    if (${status} == 'FAILURE')
+    slackSend channel: 'jenkinss', message: 'Job Failed '
+    if (${status} == 'SUCCESS') 
+    slackSend channel: 'jenkinss', message: 'Job Build successfully'
 }
 
 
