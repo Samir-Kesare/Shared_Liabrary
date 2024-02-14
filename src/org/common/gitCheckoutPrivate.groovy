1fec39b4f7d92c5edf3ce3be554809756f647a89
Vishalkk1998 <156056413+Vishalkk1998@users.noreply.github.com>
2024-02-14 23:20:16 +0530

    Update gitCheckoutPrivate.groovy

--- src/org/common/gitCheckoutPrivate.groovy
+++ src/org/common/gitCheckoutPrivate.groovy
@@ -1,1 +1,9 @@
+package org.common
 
+def call(String url, String creds, String branch) {
+    stage('Clone') {
+        script {
+            git branch: "${branch}", credentialsId: "${creds}", url: "${url}"
+        }
+    }
+}
