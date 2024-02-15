0b3135d8b0ca45f673df8f1fd274066179bc564c
Vishalkk1998 <156056413+Vishalkk1998@users.noreply.github.com>
2024-02-15 13:53:44 +0530

    Update GolangUnitTesting.groovy

--- src/org/avengers/template/GolangUnitTesting.groovy
+++ src/org/avengers/template/GolangUnitTesting.groovy
@@ -4,6 +4,14 @@ import org.avengers.common.*
 import org.avengers.golang.unitTesting.*
 
 def call(String url, String creds, String branch){
-  installGo
+  installGo = new InstallGo()
+  gitCheckoutPrivate = new GitCheckoutPrivate()
+  testing = new Testing()
+  html = Html()
+
+  installGo.call()
+  gitCheckoutPrivate.call(url, creds, branch)
+  testing.call()
+  html.call()
 }
   
