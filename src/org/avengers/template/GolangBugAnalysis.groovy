1426563bb77eb55bc26c170326825bdb78947a47
Vishalkk1998 <156056413+Vishalkk1998@users.noreply.github.com>
2024-02-15 16:30:21 +0530

    Create GolangBugAnalysis.groovy

---
+++ src/org/avengers/template/GolangBugAnalysis.groovy
@@ -0,0 +1,17 @@
+package org.avengers.template
+
+import org.avengers.common.*
+import org.avengers.golang.bugAnalysis.*
+
+def call(String url, String creds, String branch){
+  installationPreRequisites = new InstallationPreRequisites()
+  gitCheckoutPrivate = new GitCheckoutPrivate()
+  linting = new Linting()
+  report = new Report()
+
+  installationPreRequisites.call()
+  gitCheckoutPrivate.call(url, creds, branch)
+  linting.call()
+  report.call()
+}
+  
