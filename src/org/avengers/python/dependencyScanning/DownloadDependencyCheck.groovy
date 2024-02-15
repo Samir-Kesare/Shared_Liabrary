5536d87dfd5d46c2b957f9f0b697a385682767b2
Vishalkk1998 <156056413+Vishalkk1998@users.noreply.github.com>
2024-02-15 23:51:21 +0530

    Update DownloadDependencyCheck.groovy

--- src/org/avengers/python/dependencyScanning/DownloadDependencyCheck.groovy
+++ src/org/avengers/python/dependencyScanning/DownloadDependencyCheck.groovy
@@ -2,12 +2,10 @@ package org.avengers.python.dependencyScanning
 
 def call(String depVersion) {
     stage('Download Dependency Check') {
-        steps {
             script {
                 sh "wget -q https://github.com/jeremylong/DependencyCheck/releases/download/v${depVersion}/dependency-check-${depVersion}-release.zip"
                 sh "sudo apt install unzip -y"
                 sh "unzip -q dependency-check-${depVersion}-release.zip"
             }
-        }
     }
 }
