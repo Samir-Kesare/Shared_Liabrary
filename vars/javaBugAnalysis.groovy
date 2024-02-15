7f85dfabce1433ffde980240ded926b77af88c50
Ninja-07 <156056709+Parasharam-Desai@users.noreply.github.com>
2024-02-15 16:15:55 +0530

    Update javaBugAnalysis.groovy

--- vars/javaBugAnalysis.groovy
+++ vars/javaBugAnalysis.groovy
@@ -1,1 +1,33 @@
+def checkoutStage() {
+    stage("Git Checkout") {
+        steps {
+            git branch: 'main', url: 'https://github.com/Parasharam-Desai/salary-api.git'
+        }
+    }
+}
+
+def bugAnalysisStage() {
+    stage("Bug Analysis") {
+        steps {
+            sh 'mvn compile'
+            sh 'mvn spotbugs:spotbugs'
+            sh 'mvn site'
+        }
+    }
+}
+
+def htmlReportStage() {
+    stage('Publish HTML Report') {
+        steps {
+            publishHTML(target: [
+                allowMissing: false,
+                alwaysLinkToLastBuild: true,
+                keepAll: true,
+                reportDir: 'target/site',
+                reportFiles: 'spotbugs.html',
+                reportName: 'SpotBugs Report'
+            ])
+        }
+    }
+}
 
