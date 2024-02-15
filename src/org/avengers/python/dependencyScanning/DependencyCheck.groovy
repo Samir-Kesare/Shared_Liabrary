13fdf1de6cbe9ff8bd954945327f2a495330569f
Vishalkk1998 <156056413+Vishalkk1998@users.noreply.github.com>
2024-02-15 23:44:58 +0530

    Update DependencyCheck.groovy

--- src/org/avengers/python/dependencyScanning/DependencyCheck.groovy
+++ src/org/avengers/python/dependencyScanning/DependencyCheck.groovy
@@ -3,7 +3,7 @@ package org.avengers.python.dependencyScanning
 def call() {
     stage('Run Dependency Check') {
         script {
-           sh 'dependency-check/bin/dependency-check.sh --scan /var/lib/jenkins/workspace/ --out dep-check.html'
+           sh "dependency-check/bin/dependency-check.sh --scan /var/lib/jenkins/workspace/ --out dep-check.html"
         }
     }
 }
