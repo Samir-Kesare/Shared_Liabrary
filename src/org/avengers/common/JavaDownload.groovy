646f8cb93fb3c2619b6e442513602e3af89e5320
Vishalkk1998 <156056413+Vishalkk1998@users.noreply.github.com>
2024-02-15 18:28:59 +0530

    Update JavaDownload.groovy

--- src/org/avengers/common/JavaDownload.groovy
+++ src/org/avengers/common/JavaDownload.groovy
@@ -1,1 +1,9 @@
+package org.avengers.common
 
+def call(String javaversion) {
+    stage('Clone') {
+        script {
+            git branch: "${branch}", credentialsId: "${creds}", url: "${url}"
+        }
+    }
+}
