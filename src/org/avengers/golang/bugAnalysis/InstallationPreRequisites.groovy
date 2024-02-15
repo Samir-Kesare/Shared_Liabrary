03b2f617f3ddfca0b81837d9b7099dc9895e0ba9
Vishalkk1998 <156056413+Vishalkk1998@users.noreply.github.com>
2024-02-15 16:19:01 +0530

    Update InstallationPreRequisites.groovy

--- src/org/avengers/golang/bugAnalysis/InstallationPreRequisites.groovy
+++ src/org/avengers/golang/bugAnalysis/InstallationPreRequisites.groovy
@@ -1,1 +1,19 @@
 
+package org.avengers.golang.bugAnalysis
+
+def call() {
+    stage('Installation Pre-Requisites') {
+        script {
+            // Update apt packages
+            sh 'sudo apt update'
+            // Install Go using snap
+            sh 'sudo snap install go --classic'
+            // Remove golangci-lint using snap
+            sh 'sudo snap remove golangci-lint || true'
+            // Install GolangCI-lint
+            sh 'go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest'
+            // Add $HOME/go/bin to PATH
+            env.PATH += ":$HOME/go/bin"
+        }
+    }
+}
